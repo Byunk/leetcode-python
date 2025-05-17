b3c039da802bba9d9ccb22dcd1601fa6c1527f7c
Kyungho Byoun <kyungho.byoun@gmail.com>
2025-05-18 00:20:42 +0900

    Time: 0 ms (100%), Space: 17.8 MB (84.53%) - LeetHub

--- LeetCode/Medium/0002-add-two-numbers/0002-add-two-numbers.py
+++ LeetCode/Medium/0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,40 +5,23 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        head = ListNode()
-        curr = head
+        head = curr = ListNode()
         carried = 0
-        while l1 and l2:
-            s = l1.val + l2.val + carried
+        while l1 or l2 or carried:
+            s = carried
+            if l1:
+                s += l1.val
+                l1 = l1.next
+            if l2:
+                s += l2.val
+                l2 = l2.next
+
             if s >= 10:
                 carried, s = 1, s - 10
             else:
                 carried = 0
             
             curr.next = ListNode(val=s)
-            curr, l1, l2 = curr.next, l1.next, l2.next
-        
-        while l1:
-            s = l1.val + carried
-            if s >= 10:
-                carried, s = 1, s - 10
-            else:
-                carried = 0
-
-            curr.next = ListNode(val=s)
-            curr, l1 = curr.next, l1.next
-
-        while l2:
-            s = l2.val + carried
-            if s >= 10:
-                carried, s = 1, s - 10
-            else:
-                carried = 0
-
-            curr.next = ListNode(val=s)
-            curr, l2 = curr.next, l2.next
-
-        if carried == 1:
-            curr.next = ListNode(val=1)
+            curr = curr.next
         
         return head.next
